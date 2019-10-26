6fc469dca87ceb41ae29977b5510d6128549e103
hoshinosuna <56984691+hoshinosuna@users.noreply.github.com>
2019-10-27 00:49:42 +0900

    load header

--- js/common.js
+++ js/common.js
@@ -1,3 +1,7 @@
 $(function(){
   $("#footer").load("https://hoshinosuna.github.io/html/footer.html");
 });
+
+$(function(){
+  $("#header").load("https://hoshinosuna.github.io/html/header.html");
+});
