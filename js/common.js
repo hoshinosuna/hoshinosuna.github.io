c80c54db802a50b79e2662173f1db288c224eabe
hoshinosuna <56984691+hoshinosuna@users.noreply.github.com>
2019-10-27 00:53:00 +0900

    navi open

--- js/common.js
+++ js/common.js
@@ -5,3 +5,18 @@ $(function(){
 $(function(){
   $("#header").load("https://hoshinosuna.github.io/html/header.html");
 });
+
+
+jQuery(function($) {
+    $(function() {
+        $("#nav-open").on("click", function() {
+            if ($(this).hasClass("active")) {
+                $(this).removeClass("active");
+                $("#nav-content").removeClass("open").fadeOut(100);
+            } else {
+                $(this).addClass("active");
+                $("#nav-content").fadeIn(100).addClass("open");
+            }
+        });
+    });
+});
