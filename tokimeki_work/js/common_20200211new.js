b35c178d2593b572030c771ea1a1407dc77f41ec
hoshinosuna <56984691+hoshinosuna@users.noreply.github.com>
2020-02-11 12:03:52 +0900

    Add files via upload

---
+++ tokimeki_work/js/common_20200211new.js
@@ -0,0 +1,7 @@
+$(function(){
+  $("#footer").load("https://hoshinosuna.github.io/tokimeki_work/html/footer_20200211new.html");
+});
+
+$(function(){
+  $("#header").load("https://hoshinosuna.github.io/html/header.html");
+});
