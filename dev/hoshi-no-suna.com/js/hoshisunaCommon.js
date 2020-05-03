b2331a5555fc112dcef1828da6081831baefc895
hoshinosuna <hoshi.suna.space@gmail.com>
2020-05-04 00:01:27 +0900

    PLAN -0.9

--- dev/hoshi-no-suna.com/js/hoshisunaCommon.js
+++ dev/hoshi-no-suna.com/js/hoshisunaCommon.js
@@ -2,5 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('flow-toggle-btn')
         .addEventListener('click', function() {
             document.querySelector('.flow-list').classList.toggle('is-open');
+            document.querySelector('#flow-toggle-btn').classList.toggle('is-open');
         });
 }, false);
