862fa5469726803dcd69526cdcd7267a030b3439
hoshinosuna <56984691+hoshinosuna@users.noreply.github.com>
2020-05-09 17:57:00 +0900

    Notice 臨時休業のお知らせについて追加

--- dev/hoshi-no-suna.com/js/hoshisunaCommon.js
+++ dev/hoshi-no-suna.com/js/hoshisunaCommon.js
@@ -1,23 +1,29 @@
 // FLOW
 document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById('flow-toggle-btn')
-    .addEventListener('click', function() {
-      document.querySelector('.flow-list').classList.toggle('is-open');
-      document.querySelector('#flow-toggle-btn').classList.toggle('is-open');
-    });
+    document.getElementById('flow-toggle-btn')
+        .addEventListener('click', function() {
+            document.querySelector('.flow-list').classList.toggle('is-open');
+            document.querySelector('#flow-toggle-btn').classList.toggle('is-open');
+        });
+}, false);
+
+// notice
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('close-notice')
+        .addEventListener('click', function() {
+            document.querySelector('#notice').classList.toggle('close');
+            document.querySelector('#header-section').classList.toggle('hide');
+        });
+
+    var isHiddenMenu = document.getElementById('hidden-menu-checkbox1').checked;
+    if (isHiddenMenu) {
+        document.querySelector('#header-section').classList.toggle('hide');
+    }
+
 }, false);
 
 
-
 // FAQ
-// $(function() {
-//   $('.question').on('click', function() {
-//     $(this).next('.answer').slideToggle();
-//     $(this).toggleClass('open');
-//   });
-// });
-
-
 function toggleAnswer(num) {
-  document.querySelector('#question' + num).classList.toggle('open');
+    document.querySelector('#question' + num).classList.toggle('open');
 }
